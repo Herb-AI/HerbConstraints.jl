58be818ce4f5ae397c213a0b7c2a5c54b51b3b5f
Jaap de Jong <jaapdejong15@gmail.com>
2023-03-08 17:00:11 +0100

    Add constraint that forbids a (sub)tree to occur in the search. Also includes a new datastructure for specifying the forbidden trees.

--- src/HerbConstraints.jl
+++ src/HerbConstraints.jl
@@ -4,12 +4,17 @@ using ..HerbGrammar
 
 abstract type PropagatorConstraint <: Constraint end
 
+include("constraintmatchnode.jl")
+
 include("comesafter.jl")
 include("forbidden.jl")
 include("ordered.jl")
 include("forbidden_tree.jl")
 
 export
+    ConstraintMatchNode,
+    ConstraintMatchVar,
+
     PropagatorConstraint,
 
     propagate,
---
+++ src/constraintmatchnode.jl
@@ -0,0 +1,15 @@
+"""
+Tree structure to which rulenode trees can be matched.
+"""
+abstract type AbstractConstraintMatchNode end
+
+struct ConstraintMatchNode <: AbstractConstraintMatchNode
+    rule_ind::Int
+    children::Vector{AbstractConstraintMatchNode}
+end
+
+ConstraintMatchNode(rule_ind::Int) = ConstraintMatchNode(rule_ind, [])
+
+struct ConstraintMatchVar <: AbstractConstraintMatchNode
+    variable_name::Symbol
+end
