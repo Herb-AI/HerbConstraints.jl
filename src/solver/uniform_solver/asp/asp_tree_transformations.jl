59135e6418ba3db32778d03c3926e6f966f9aaaf
Sebastijan <sebastijan.dumancic@gmail.com>
2025-04-15 08:29:56 +0200

    tmp ASP solver

---
+++ src/solver/uniform_solver/asp/asp_tree_transformations.jl
@@ -0,0 +1,130 @@
+"""
+UTILITIES for tree -> ASP transformations
+"""
+
+
+"""
+  tree_to_ASP(tree::AbstractRuleNode, grammar::AbstractGrammar, node_index::Int64)
+
+Transforms a [UniformTree] into an ASP program.
+Nodes get their IDs based on the in-order traversal.
+
+Examples:
+
+@rulenode 4{1,2}
+->
+node(1,4).
+child(1,1,2).
+node(2,1).
+child(1,2,3).
+node(3,2).
+
+@rulenode [4,5]{1,2} ->
+1 { node(1,4);node(1,5) } 1.
+child(1,1,2).
+node(2,1).
+child(1,2,3).
+node(3,2).
+
+@rulenode [4,5]{[1,2,3],[1,2,3]} ->
+1 { node(1,4);node(1,5) } 1.
+child(1,1,2).
+1 { node(2,1);node(2,2);node(2,3) } 1.
+child(1,2,3).
+1 { node(3,1);node(3,2);node(3,3) } 1.
+
+"""
+function tree_to_ASP(tree::AbstractRuleNode, grammar::AbstractGrammar, node_index::Int64)
+    output = ""
+    output *= node_to_ASP(tree, grammar, node_index)
+    parent_index = node_index
+    node_index = node_index + 1
+    for (child_ind, child) in enumerate(tree.children)
+        output *= "child($(parent_index),$(child_ind),$(node_index)).\n"
+        ch_output, node_index = tree_to_ASP(child, grammar, node_index)
+        output *= ch_output
+    end
+    return output, node_index
+end
+
+"""
+  node_to_ASP(tree::RuleNode, grammar::AbstractGrammar, node_index::Int64)
+
+Transforms a [RuleNode] into an ASP representation in the form `node(node_index, rule_id)`.
+"""
+function node_to_ASP(tree::RuleNode, grammar::AbstractGrammar, node_index::Int64)
+    return "node($(node_index),$(get_rule(tree))).\n"
+end
+
+"""
+  node_to_ASP(tree::Union{UniformHole,DomainRuleNode}, grammar::AbstractGrammar, node_index::Int64)
+
+Transforms a [UniformHole] or [DomainRuleNode] into an ASP representation in the form
+`1 { node(node_index, rule_id_1); node(node_index, rule_id_2);...} 1.`.
+"""
+function node_to_ASP(tree::Union{UniformHole,DomainRuleNode}, grammar::AbstractGrammar, node_index::Int64)
+    options = join(["node($(node_index),$(ind))" for ind in filter(x -> tree.domain[x], 1:length(grammar.rules))], ";")
+    return "1 { $(options) } 1.\n"
+end
+
+
+function node_to_ASP(tree::StateHole, grammar::AbstractGrammar, node_index::Int64)
+    options = join(["node($(node_index),$(ind))" for ind in Base.findall(tree.domain)], ";")
+    return "1 { $(options) } 1.\n"
+end
+
+
+"""
+    constraint_tree_to_ASP(grammar::AbstractGrammar, tree::AbstractRuleNode, node_index::Int64)
+
+Transforms a template tree to an ASP form suitable for constraints.
+
+@rulenode 5{3,3} -> node(X1,5), child(X1,1,X2),node(X2,3), child(X1,2,X3),node(X3,3)
+
+@rulenode [4,5]{3,3} -> allowed(x1,1). node(X1,D1),allowed(x1,D1),child(X1,1,X2),node(X2,3),child(X1,2,X3),node(X3,3).
+"""
+function constraint_tree_to_ASP(grammar::AbstractGrammar, tree::AbstractRuleNode, node_index::Int64, constraint_index::Int64)
+    tree_facts, additional_facts = "", ""
+    tmp_facts, tmp_additional = constraint_node_to_ASP(grammar, tree, node_index, constraint_index::Int64)
+    tree_facts *= tmp_facts
+    additional_facts *= join(tmp_additional, "")
+    parent_index = node_index
+    node_index += 1
+
+    for (child_ind, child) in enumerate(tree.children)
+        if isa(child, VarNode)
+            tree_facts *= ",child(X$(parent_index),$(child_ind),$(child.name))"
+        else
+            tmp_facts, tmp_additional = constraint_tree_to_ASP(grammar, child, node_index, constraint_index)
+            tree_facts *= ",child(X$(parent_index),$(child_ind),X$(node_index))"
+            tree_facts *= ",$(tmp_facts)"
+            additional_facts *= join(tmp_additional, "")
+            node_index += 1
+        end
+    end
+
+    return tree_facts, additional_facts, node_index
+end
+
+"""
+    constraint_node_to_ASP(grammar::AbstractGrammar, node::RuleNode, node_index::Int64)
+
+Transforms a RuleNode into an ASP shape for a constraint
+"""
+function constraint_node_to_ASP(grammar::AbstractGrammar, node::RuleNode, node_index::Int64, constraint_index::Int64)
+    return "node(X$(node_index),$(get_rule(node)))", []
+end
+
+"""
+    constraint_node_to_ASP(grammar::AbstractGrammar, node::RuleNode, node_index::Int64)
+
+Transforms a RuleNode into an ASP shape for a constraint
+"""
+function constraint_node_to_ASP(grammar::AbstractGrammar, node::Union{UniformHole,DomainRuleNode}, node_index::Int64, constraint_index::Int64)
+    return "node(X$(node_index),D$(node_index)),allowed(c$(constraint_index)x$(node_index),D$(node_index))", map(x -> "allowed(c$(constraint_index)x$(node_index), $x).\n", collect(filter(x -> node.domain[x], 1:length(grammar.rules))))
+end
+
+
+function constraint_node_to_ASP(grammar::AbstractGrammar, node::StateHole, node_index::Int64, constraint_index::Int64)
+    return "node(X$(node_index),D$(node_index)),allowed(c$(constraint_index)x$(node_index),D$(node_index))", map(x -> "allowed(c$(constraint_index)x$(node_index), $x).\n", Base.findall(node.domain))
+end
--- src/solver/uniform_solver/asp/asp_uniform_tree_solver.jl
+++ src/solver/uniform_solver/asp/asp_uniform_tree_solver.jl
@@ -1,125 +1,63 @@
-using HerbCore, HerbGrammar, HerbConstraints, Clingo_jll
-include("parsing_IO.jl")
-
-g = @csgrammar begin
-    Number = |(1:2)
-    Number = x
-    Number = Number + Number
-    Number = Number * Number
+struct ASPSolver <: Solver
+    grammar::AbstractGrammar
+    tree::Union{RuleNode,StateHole}
+    solutions::Vector{Dict{Int64,Int64}} #vector of dictionaries with key=node and value=matching rule index
+    tmp_file_name::String
 end
 
-addconstraint!(g, Forbidden(RuleNode(5, [RuleNode(3), RuleNode(3)])))
-addconstraint!(g, Forbidden(RuleNode(5, [UniformHole(BitVector((1, 1, 1, 0, 0)), []), UniformHole(BitVector((1, 1, 0, 0, 0)), [])])))
-addconstraint!(g, Forbidden(RuleNode(5, [RuleNode(4, [RuleNode(1), RuleNode(2)]), RuleNode(3)])))
+get_name(::ASPSolver) = "ASPSolver"
 
-addconstraint!(g, Contains(4))
-addconstraint!(g, ContainsSubtree(RuleNode(4, [UniformHole(BitVector((1, 1, 0, 0, 0)), []), RuleNode(3)])))
+get_tree(solver::ASPSolver) = solver.tree
 
-addconstraint!(g, Unique(5))
+get_grammar(solver::ASPSolver) = solver.grammar
 
-addconstraint!(g, ForbiddenSequence(Vector{Int}([5,4,3])))
-addconstraint!(g, ForbiddenSequence(Vector{Int}([5,1])))
+get_filename(solver::ASPSolver) = solver.tmp_file_name
 
+ASPSolver(grammar, tree) = ASPSolver(grammar, tree, Vector{Dict{Int32,Int32}}(), "__tmp_asp_file.lp")
 
-t1 = RuleNode(4, [RuleNode(1), RuleNode(2)])
-t2 = DomainRuleNode(BitVector((0, 0, 0, 1, 1)), [RuleNode(1), RuleNode(2)])
-t3 = UniformHole(BitVector((0, 0, 0, 1, 1)), [UniformHole(BitVector((1, 1, 1, 0, 0)), []), UniformHole(BitVector((1, 1, 1, 0, 0)), [])])
-t4 = UniformHole(BitVector((0, 0, 0, 1, 1)), [UniformHole(BitVector((0,0,0,1,1)), [UniformHole(BitVector((1, 1, 1, 0, 0)), []), UniformHole(BitVector((1, 1, 1, 0, 0)), [])]), UniformHole(BitVector((1,1,1,0,0)), [])])
+function prepare_ASP(solver::ASPSolver)
+    string_tree, _ = tree_to_ASP(get_tree(solver), get_grammar(solver), 1)
 
-
-function parse_tree(tree::AbstractRuleNode, grammar::AbstractGrammar, output::AbstractString, node_index::Int64)
-    if tree isa RuleNode
-        output *= "node($(node_index),$(tree.ind)).\n"
-    elseif tree isa UniformHole || tree isa DomainRuleNode
-        options = join(["node($(node_index),$(ind))" for ind in filter(x -> tree.domain[x], 1:length(grammar.rules))], ";")
-        output *= "1 { $(options) } 1.\n"
+    open(get_filename(solver), "w") do file
+        write(file, "%%% Tree\n")
+        write(file, string_tree)
+        write(file, "\n%%% Constraints\n")
+        constraints = to_ASP(get_grammar(solver))
+        write(file, constraints)
+        write(file, "#show node/2.\n")
     end
-    parent_idx = node_index
-    node_index += 1
-    for (child_idx, child) in enumerate(tree.children)
-        output *= "child($(parent_idx),$(child_idx),$(node_index)).\n"
-        output, node_index = parse_tree(child, grammar, output, node_index)
-    end
-    return output, node_index
 end
 
-function parse_tree_var(tree::AbstractRuleNode, grammar::AbstractGrammar, output::Vector{AbstractString}, domains::Vector{AbstractString}, node_index::Int64)
-    parent = "X$(node_index)"
-    if tree isa RuleNode
-        push!(output, "node($(parent),$(tree.ind))")
-    elseif tree isa UniformHole || tree isa DomainRuleNode
-        push!(output, "node($(parent),$(parent)D), allowed(x$(node_index)d,$(parent)D)")
-        for ind in filter(x -> tree.domain[x], 1:length(grammar.rules))
-            push!(domains, "allowed(x$(node_index)d,$(ind))")
+get_resultsfile(solver::ASPSolver) = replace(get_filename(solver), ".lp" => "_res.lp")
+
+function extract_solutions!(solver::ASPSolver)
+    open(get_resultsfile(solver), "r") do file
+        current_solution = Dict{Int64,Int64}()
+        for line in eachline(file)
+            if startswith(line, "Answer")
+                if !isempty(current_solution)
+                    push!(solver.solutions, current_solution)
+                end
+                current_solution = Dict{Int64,Int64}()
+            elseif startswith(line, "node")
+                node_assignments = split(line, " ")
+                for node in node_assignments
+                    m = match(r"node\((\d+),(\d+)\)", node)
+                    current_solution[parse(Int, m.captures[1])] = parse(Int, m.captures[2])
+                end
+            elseif startswith(line, "SATISFIABLE")
+                break
+            end
         end
     end
-    node_index += 1
-    for (child_idx, child) in enumerate(tree.children)
-        push!(output, "child($(parent),$(child_idx),X$(node_index))")
-        output, domains, node_index = parse_tree_var(child, grammar, output, domains, node_index)
-    end
-    return output, domains, node_index
 end
 
-function parse_constraints(grammar::AbstractGrammar)
-    output = ""
-    for (index, constraint) in enumerate(grammar.constraints)
-        output *= parse_constraint(grammar, constraint, index)
-    end
-    return output
+function find_solutions!(solver::ASPSolver)
+    prepare_ASP(solver)
+
+    source_file = get_filename(solver)
+    res_file = get_resultsfile(solver)
+    run(`clingo --models 0 ./$source_file \> ./$res_file`)
+
+    extract_solutions!(solver)
 end
-
-function parse_constraint(grammar::AbstractGrammar, constraint::Forbidden, index::Int64)
-    tree, domains, _ = parse_tree_var(constraint.tree, grammar, Vector{AbstractString}(), Vector{AbstractString}(), 1)
-
-    # % :- node(X,5), child(X,1,Y), node(Y,YD), allowed(yD,YD).
-    # % allowed(yD,3).
-    # % allowed(yD,2).
-    # % allowed(yD,1).    
-    output = ":- $(join(tree, ", ")).\n"
-    for d in domains
-        output *= "$(d).\n"
-    end
-    return output
-end
-
-function parse_constraint(grammar::AbstractGrammar, constraint::Contains, index::Int64)
-    return ":- not node(_,$(constraint.rule)).\n"
-end
-
-function parse_constraint(grammar::AbstractGrammar, constraint::Unique, index::Int64)
-    return "{ X : node(X,$(constraint.rule)) } 1."
-end
-
-function parse_constraint(grammar::AbstractGrammar, constraint::ContainsSubtree, index::Int64)
-    tree, domains, _ = parse_tree_var(constraint.tree, grammar, Vector{AbstractString}(), Vector{AbstractString}(), 1)
-    output = "subtree(c$(index)) :- $(join(tree, ", ")).\n:- not subtree(c$(index)).\n"
-    for d in domains
-        output *= "$(d).\n"
-    end
-    return output    
-end
-
-function parse_constraint(grammar::AbstractGrammar, constraint::ForbiddenSequence, index::Int64)
-    ## TODO implement
-end
-
-function parse_constraint(grammar::AbstractGrammar, constraint::Ordered, index::Int64)
-    tree, domains, _ = parse_tree_var(constraint.tree, grammar, Vector{AbstractString}(), Vector{AbstractString}(), 1)
-
-end
-
-
-function solve_uniform_tree(tree::AbstractRuleNode, grammar::AbstractGrammar)
-    println("%%% Tree")
-    string_tree, _ = parse_tree(tree, grammar, "", 1)
-    println(string_tree)
-    println("%%% Constraint")
-    constraints = parse_constraints(grammar)
-    println(constraints)
-end
-
-solve_uniform_tree(t1,g)
-solve_uniform_tree(t2,g)
-solve_uniform_tree(t3,g)
-solve_uniform_tree(t4,g)
